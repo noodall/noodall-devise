45b2af96603730275fda2e6c7aa41335fdc9d67a
Daniel Craig <daniel@wearebeef.co.uk>
2011-01-27 18:37:31 +0000

    change assets to menu init

--- lib/noodall/devise/engine.rb
+++ lib/noodall/devise/engine.rb
@@ -1,7 +1,7 @@
 module Noodall
   module Devise
     class Engine < Rails::Engine
-      initializer "static assets" do |app|
+      initializer "set menu" do |app|
         Noodall::UI.menu_items['Users'] = :admin_users_path
       end
     end
