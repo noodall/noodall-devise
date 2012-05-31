dd7b367cea018b754a88e3808604a91024e6d54e
Liam Richardson <discoliam@googlemail.com>
2012-05-31 17:02:40 +0100

    styled form for user

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -2,9 +2,8 @@ module Admin::UsersHelper
   include SortableTable::App::Helpers::ApplicationHelper
 
   def password_label
-    text = 'Password'
     if User.respond_to? :password_length and User.password_length.kind_of? Range
-      text += " (must be between #{User.password_length.first} and #{User.password_length.last} characters long)"
+      text = " (must be between #{User.password_length.first} and #{User.password_length.last} characters long)"
     end
     return text
   end
